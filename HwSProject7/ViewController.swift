2b7310702ae28740e6ed3351e4e2ddf60ddc785f
Skyler Svendsen <skylersvendsen@skylers-mbp.lan>
2017-08-22 21:44:53 -0500

    Initial Commit

---
+++ HwSProject7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HwSProject7
+//
+//  Created by Skyler Svendsen on 8/22/17.
+//  Copyright © 2017 Skyler Svendsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
